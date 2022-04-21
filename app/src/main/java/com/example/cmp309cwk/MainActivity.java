6edc445c3fc2e64fc072e1dc380e0247f968d3d9
Ewan <ewanstewart04@outlook.com>
2022-04-21 13:27:01 +0100

    Add the location permission check

--- app/src/main/java/com/example/cmp309cwk/MainActivity.java
+++ app/src/main/java/com/example/cmp309cwk/MainActivity.java
@@ -2,7 +2,9 @@ package com.example.cmp309cwk;
 
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.Manifest;
 import android.content.Intent;
+import android.content.pm.PackageManager;
 import android.os.*;
 
 public class MainActivity extends AppCompatActivity {
@@ -11,8 +13,8 @@ public class MainActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
-
     }
 }
+
 
 
