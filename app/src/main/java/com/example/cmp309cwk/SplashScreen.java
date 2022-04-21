cf2534abf79ae3fa76e9f106f600d59617b75cdb
Ewan <ewanstewart04@outlook.com>
2022-04-21 13:27:01 +0100

    Location permission check

--- app/src/main/java/com/example/cmp309cwk/SplashScreen.java
+++ app/src/main/java/com/example/cmp309cwk/SplashScreen.java
@@ -2,17 +2,16 @@ package com.example.cmp309cwk;
 
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.Manifest;
 import android.content.Intent;
+import android.content.pm.PackageManager;
 import android.os.Bundle;
 import android.os.Handler;
+import android.util.Log;
 
 public class SplashScreen extends AppCompatActivity {
 
-    @Override
-    protected void onCreate(Bundle savedInstanceState) {
-        super.onCreate(savedInstanceState);
-        setContentView(R.layout.activity_splash_screen);
-        getSupportActionBar().hide();
+    public void openMainActivity() {
         new Handler().postDelayed(new Runnable() {
             @Override
             public void run() {
@@ -20,6 +19,37 @@ public class SplashScreen extends AppCompatActivity {
                 startActivity(intent);
                 finish();
             }
-        } , 3000);
+        } , 2000);
     }
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_splash_screen);
+        getSupportActionBar().hide();
+
+        //ask for coarse location permission in order to open the main activity
+        if (checkSelfPermission(Manifest.permission.ACCESS_COARSE_LOCATION) != PackageManager.PERMISSION_GRANTED) {
+            requestPermissions(new String[]{Manifest.permission.ACCESS_COARSE_LOCATION}, 1);
+        } else {
+            openMainActivity();
+        }
+    }
+
+    @Override
+    public void onRequestPermissionsResult(int requestCode, String[] permissions, int[] grantResults) {
+        super.onRequestPermissionsResult(requestCode, permissions, grantResults);
+        if (grantResults.length > 0 && grantResults[0] == PackageManager.PERMISSION_GRANTED) {
+            openMainActivity();
+        } else {
+            //ask for permission again
+            if (checkSelfPermission(Manifest.permission.ACCESS_COARSE_LOCATION) != PackageManager.PERMISSION_GRANTED) {
+                requestPermissions(new String[]{Manifest.permission.ACCESS_COARSE_LOCATION}, 1);
+            } else {
+                openMainActivity();
+            }
+        }
+    }
+
+
 }
