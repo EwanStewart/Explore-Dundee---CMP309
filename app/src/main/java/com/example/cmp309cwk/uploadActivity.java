eb4bfacbe1d39f3f65315e956c8188603125f091
Ewan <ewanstewart04@outlook.com>
2022-05-07 13:51:26 +0100

    update

--- app/src/main/java/com/example/cmp309cwk/uploadActivity.java
+++ app/src/main/java/com/example/cmp309cwk/uploadActivity.java
@@ -1,22 +1,35 @@
 package com.example.cmp309cwk;
 
-import androidx.appcompat.app.AppCompatActivity;
-
 import android.os.Bundle;
-import android.util.Log;
 import android.widget.TextView;
 
+import androidx.appcompat.app.AppCompatActivity;
+
+import com.google.android.gms.maps.CameraUpdateFactory;
+import com.google.android.gms.maps.GoogleMap;
+import com.google.android.gms.maps.model.LatLng;
+
 public class uploadActivity extends AppCompatActivity {
+    private GoogleMap mMap;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_upload);
         getSupportActionBar().hide();
+        TextView landmarksVisitedTextView = findViewById(R.id.txtViewLandmarksVisited);
 
 
 
         Bundle bundle = getIntent().getExtras();
+
+        if(bundle.getString("landmarksVisited") != null){
+            String landmarksVisitedString = bundle.getString("landmarksVisited");
+            landmarksVisitedString = landmarksVisitedString.substring(1);
+            landmarksVisitedTextView.setText("Landmarks Visited: " + landmarksVisitedString);
+        } else {
+            landmarksVisitedTextView.setText("Landmarks Visited: You didn't visit any landmarks");
+        }
 
 
 
@@ -27,6 +40,14 @@ public class uploadActivity extends AppCompatActivity {
         String totalTime = bundle.getString("time");
         TextView timeText = findViewById(R.id.txtTotalTime);
         timeText.setText("Total time: " +  totalTime);
+
+    }
+
+    public void onMapReady(GoogleMap googleMap) {
+        mMap = googleMap;
+
+        mMap.moveCamera(CameraUpdateFactory.newLatLngZoom(new LatLng(56.46341960060735, -2.973959916879307), 15));
+
 
     }
 }
